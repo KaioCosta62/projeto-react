a330a060dda199139334af9c3baa1033f5a946fd
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-27 17:12:53 -0300

    customizando estilos do materialUI e configurando tema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App';
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+import {deepPurple, red} from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: deepPurple[800]
+    },
+    secondary: {
+      main: red[900]
+    }
+  },
+});
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-);
+)
+
