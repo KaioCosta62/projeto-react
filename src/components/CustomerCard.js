48c0d00b06da190975e75d5e4ef89e192f237c76
Kaio Henrique <kaiohenriqueranking1@gmail.com>
2022-10-30 16:23:36 -0300

    modal de deletar o usuario

--- src/components/CustomerCard.js
+++ src/components/CustomerCard.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, {useState} from 'react'
 
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
@@ -6,31 +6,55 @@ import CardActions from '@mui/material/CardActions'
 import Avatar from '@mui/material/Avatar'
 import IconButton from '@mui/material/IconButton'
 
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import ShareIcon from '@mui/icons-material/Share'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+import ModalConfirm from './ModalConfirm'
 
 const CustomerCard = ({name, lastName, email, avatar, className}) => {
+  const [openModal, setOpenModal] = useState(false)
 
+  const handleToggleOpenModal = () => {
+    setOpenModal(!openModal)
+  }
+
+  const handleModal = () => {
+    alert("Ok")
+  }
+
+  const handleRemoveCustomer = () => {
+    handleToggleOpenModal()
+  }
   return (
-    <Card sx={{ maxWidth: 345}} className = {className}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" src = {avatar}>
-            {name[0]}
-          </Avatar>
-        }
-        title= {`${name} ${lastName}`}
-        subheader= {email}
+    <>
+      <Card sx={{ maxWidth: 345}} className = {className}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" src = {avatar}>
+              {name[0]}
+            </Avatar>
+          }
+          title= {`${name} ${lastName}`}
+          subheader= {email}
+        />
+        <CardActions disableSpacing>
+          <IconButton aria-label="Editar cadastro">
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="Deletar cadastro">
+            <DeleteIcon onClick = {handleRemoveCustomer} />
+          </IconButton>
+        </CardActions>
+      </Card>
+      <ModalConfirm
+        open = {openModal}
+        onClose = {handleToggleOpenModal}
+        onConfirm = {handleModal}
+        title = 'Deseja realmente excluir esse cadastro?'
+        message = 'Ao confirmar não será possível reverter essa operação'
       />
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    </>
+   
   )
 }
 
---
+++ src/components/ModalConfirm.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+import {
+  Button, 
+  Dialog, 
+  DialogActions, 
+  DialogContent, 
+  DialogContentText,
+  DialogTitle 
+} from '@mui/material'
+
+ const ModalConfirm = ({open, onClose, onConfirm, title, message}) => {
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancelar</Button>
+          <Button onClick={onConfirm} autoFocus>
+            Excluir usuário
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+export default ModalConfirm
